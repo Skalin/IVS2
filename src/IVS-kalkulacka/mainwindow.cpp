b2075f29ee3675e4460f72a9a5c89f36827a6e49
Jan Válka <xvalka03@stud.fit.vutbr.cz>
2018-03-16 18:56:27 +0100

    added buttons behaviour

--- src/IVS-kalkulacka/mainwindow.cpp
+++ src/IVS-kalkulacka/mainwindow.cpp
@@ -6,6 +6,7 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    //ui->resultArea->setInputMask("9999999");
 }
 
 MainWindow::~MainWindow()
@@ -15,7 +16,105 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_numberSeven_clicked()
 {
-    ui->resultArea->moveCursor(QTextCursor::End);
-    ui->resultArea->insertPlainText("7");
-    ui->resultArea->moveCursor(QTextCursor::End);
+    ui->resultArea->setText(ui->resultArea->text()+ "7");
+}
+
+
+void MainWindow::on_allClear_clicked()
+{
+    ui->resultArea->setText("");
+}
+
+
+void MainWindow::on_deleteButton_clicked()
+{
+    QString tmp;
+    tmp = ui->resultArea->text();
+    tmp.resize(tmp.size()-1);
+    ui->resultArea->setText(tmp);
+}
+
+void MainWindow::on_numberEight_clicked()
+{
+    ui->resultArea->setText(ui->resultArea->text()+ "8");
+}
+
+void MainWindow::on_numberNine_clicked()
+{
+    ui->resultArea->setText(ui->resultArea->text()+ "9");
+}
+
+void MainWindow::on_numberFour_clicked()
+{
+   ui->resultArea->setText(ui->resultArea->text()+ "4");
+}
+
+void MainWindow::on_numberFive_clicked()
+{
+    ui->resultArea->setText(ui->resultArea->text()+ "5");
+}
+
+void MainWindow::on_numberSix_clicked()
+{
+    ui->resultArea->setText(ui->resultArea->text()+ "6");
+}
+
+void MainWindow::on_numberOne_clicked()
+{
+    ui->resultArea->setText(ui->resultArea->text()+ "1");
+}
+
+void MainWindow::on_numberTwo_clicked()
+{
+    ui->resultArea->setText(ui->resultArea->text()+ "2");
+}
+
+void MainWindow::on_numberThree_clicked()
+{
+    ui->resultArea->setText(ui->resultArea->text()+ "3");
+}
+
+void MainWindow::on_numberZero_clicked()
+{
+     ui->resultArea->setText(ui->resultArea->text()+ "0");
+}
+
+void MainWindow::on_decimalPoint_clicked()
+{
+     ui->resultArea->setText(ui->resultArea->text()+ ".");
+}
+
+void MainWindow::on_sqrtSign_clicked()
+{
+    ui->resultArea->setText(ui->resultArea->text()+ "√");
+}
+
+void MainWindow::on_sqrSign_clicked()
+{
+    ui->resultArea->setText(ui->resultArea->text()+ "^");
+}
+
+void MainWindow::on_factorialSign_clicked()
+{
+    ui->resultArea->setText(ui->resultArea->text()+ "!");
+}
+
+void MainWindow::on_divideSign_clicked()
+{
+     ui->resultArea->setText(ui->resultArea->text()+ "/");
+}
+
+void MainWindow::on_multiplySign_clicked()
+{
+    ui->resultArea->setText(ui->resultArea->text()+ "*");
+}
+
+void MainWindow::on_minusSign_clicked()
+{
+     ui->resultArea->setText(ui->resultArea->text()+ "-");
+}
+
+void MainWindow::on_plusSign_clicked()
+{
+     ui->resultArea->setText(ui->resultArea->text()+ "+");
 }
--- src/IVS-kalkulacka/mainwindow.h
+++ src/IVS-kalkulacka/mainwindow.h
@@ -18,6 +18,44 @@ public:
 private slots:
     void on_numberSeven_clicked();
 
+    void on_allClear_clicked();
+
+    void on_deleteButton_clicked();
+
+    void on_numberEight_clicked();
+
+    void on_numberNine_clicked();
+
+    void on_numberFour_clicked();
+
+    void on_numberFive_clicked();
+
+    void on_numberSix_clicked();
+
+    void on_numberOne_clicked();
+
+    void on_numberTwo_clicked();
+
+    void on_numberThree_clicked();
+
+    void on_numberZero_clicked();
+
+    void on_decimalPoint_clicked();
+
+    void on_sqrtSign_clicked();
+
+    void on_sqrSign_clicked();
+
+    void on_factorialSign_clicked();
+
+    void on_divideSign_clicked();
+
+    void on_multiplySign_clicked();
+
+    void on_minusSign_clicked();
+
+    void on_plusSign_clicked();
+
 private:
     Ui::MainWindow *ui;
 };
