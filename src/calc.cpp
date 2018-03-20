5b4a2b4f3f08cdaa307f171ed864a71c4a25051c
Dominik Skála <DominikSkala@seznam.cz>
2018-03-20 17:06:24 +0100

    bracketing of size of array in sum

--- src/calc.cpp
+++ src/calc.cpp
@@ -93,7 +93,7 @@ double advancedMath::root(double operand, int exponent) {
 
 double advancedMath::sum(double *arrayOfOperands) {
 	double sum = 0.0;
-	for (unsigned int i = 0; i < sizeof(arrayOfOperands)/sizeof(double); i++) {
+	for (unsigned int i = 0; i < (sizeof(arrayOfOperands)/sizeof(double)); i++) {
 		sum += arrayOfOperands[i];
 	}
 	return sum;
