91b65ee8d680c8eae06c5893645140ecfa5f2459
Johnczek <janhrboticky@gmail.com>
2018-03-22 19:35:07 +0100

    EDIT: Parser, adding methods

--- src/parser.cpp
+++ src/parser.cpp
@@ -1,1 +1,18 @@
+class Parser {
+public:
+	std::vector <string> getInputData() {
+		return this->inputData;
+	}
 
+	void setInputData(std::vector<string> &input) {
+		this->inputData = input;
+	}
+
+protected:
+	bool cleanInputData() {
+		while (!this->inputData.empty()) {
+			this->inputData.push_back();
+		}
+	}
+private:
+}
