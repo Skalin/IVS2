4eaacb0f09b34ac8cdd31775949e470dbfed4fb1
Johnczek <janhrboticky@gmail.com>
2018-03-22 23:11:46 +0100

    EDIT: Parser, adding methods

--- src/parser.cpp
+++ src/parser.cpp
@@ -1,20 +1,45 @@
 #include "calc.h"
 
-class Parser {
+class Parser extends Math {
 public:
-	std::vector <string> getInputData() {
-		return this->inputData;
+		
+	std::vector <string> Parser::getInputData() {
+			return this->inputData;
 	}
 
-	void setInputData(std::vector<string> &input) {
+	void Parser::setInputData(std::vector<string> &input) {
 		this->inputData = input;
 	}
 
+
+	void Parser::solve(std::vector<string> &input) {
+		this->cleanInputData();
+		this->setInputData(input);
+
+		unsigned int maxPriority = 3;
+
+		for (unsigned i = maxPriority; i > 0; i--) {
+			this->solveResult(i);
+			/* just for remembering what each part of the code does
+				this->solveResult(3); // factorials, sums and averages
+				this->solveResult(2); // powers and roots
+				this->solveResult(1); // multiplication and division
+				this->solveResult(0); // addition subtraction
+			*/
+		}
+
+	}
+
 protected:
-	bool cleanInputData() {
+private:
+	bool Parser::cleanInputData() {
 		while (!this->inputData.empty()) {
 			this->inputData.push_back();
 		}
 	}
-private:
+
+	void Parser::solveResult(unsigned int priority) {
+		
+	}
+
 }
