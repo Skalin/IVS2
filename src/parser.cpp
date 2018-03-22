e102694d4d1bf029cd7872b2f5fb9a851c4c36f6
Dominik Skála <DominikSkala@seznam.cz>
2018-03-22 23:24:25 +0100

    revised parser.cpp file and replaced push_back with pop_back function in cleaning of input data

--- src/parser.cpp
+++ src/parser.cpp
@@ -1,47 +1,39 @@
-#include "calc.h"
+#include "parser.h"
 
-class Parser : public Math {
-public:
-		
-	std::vector <string> Parser::getInputData() {
-			return this->inputData;
+std::vector <std::string> Parser::getInputData() {
+		return this->inputData;
+}
+
+void Parser::setInputData(std::vector<std::string> &input) {
+	this->inputData = input;
+}
+
+void Parser::solve(std::vector<std::string> &input) {
+	this->cleanInputData();
+	this->setInputData(input);
+
+	unsigned int maxPriority = 3;
+
+	for (unsigned i = maxPriority; i > 0; i--) {
+		this->solveResult(i);
+		/* just for remembering what each part of the code does
+			this->solveResult(3); // factorials
+			this->solveResult(2); // powers and roots
+			this->solveResult(1); // multiplication and division
+			this->solveResult(0); // addition subtraction
+		*/
 	}
+	
+	// expected output after the for cycle should be class variable vector<string> containing only one item = result
 
-	void Parser::setInputData(std::vector<string> &input) {
-		this->inputData = input;
+}
+
+void Parser::cleanInputData() {
+	while (!this->inputData.empty()) {
+		this->inputData.pop_back();
 	}
+}
 
+void Parser::solveResult(unsigned int priority) {
 
-	void Parser::solve(std::vector<string> &input) {
-		this->cleanInputData();
-		this->setInputData(input);
-
-		unsigned int maxPriority = 3;
-
-		for (unsigned i = maxPriority; i > 0; i--) {
-			this->solveResult(i);
-			/* just for remembering what each part of the code does
-				this->solveResult(3); // factorials
-				this->solveResult(2); // powers and roots
-				this->solveResult(1); // multiplication and division
-				this->solveResult(0); // addition subtraction
-			*/
-		}
-		
-		// expected output after the for cycle should be class variable vector<string> containing only one item = result
-
-	}
-
-protected:
-private:
-	bool Parser::cleanInputData() {
-		while (!this->inputData.empty()) {
-			this->inputData.push_back();
-		}
-	}
-
-	void Parser::solveResult(unsigned int priority) {
-		
-	}
-
-};
+}
