292d5dcf08c4900ce68246889ab2eed3bc2646b5
Dominik Skála <DominikSkala@seznam.cz>
2018-03-24 00:45:59 +0100

    removed new lines in between declarations of functions in calc.h

--- src/calc.h
+++ src/calc.h
@@ -16,23 +16,14 @@ void printError(std::string error);
 class Math {
 public:
 	double addition(double operand1, double operand2);
-	
 	double subtraction(double operand1, double operand2);
-	
 	double multiplication(double operand1, double operand2);
-	
 	int multiplication(int operand1, int operand2);
-	
 	double division(double operand1, double operand2);
-	
 	double powerOf(double operand, int exponent);
-	
 	int factorial(int operand);
-	
 	double root(double operand, int exponent);
-	
 	double sum(std::vector <double> &);
-	
 	double average(std::vector <double> &);
 };
 
