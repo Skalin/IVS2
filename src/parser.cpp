2a1db9ffd1e3e34eca0dae3ade7fa3ec500eaab4
Dominik Skála <DominikSkala@seznam.cz>
2018-03-23 21:49:08 +0100

    code review of parser.cpp (improved return values and replaced direct access to variable with getter and setter)

--- src/parser.cpp
+++ src/parser.cpp
@@ -8,55 +8,35 @@ void Parser::setInputData(std::vector<std::string> &input) {
 	this->inputData = input;
 }
 
-void Parser::solve(std::vector<std::string> &input) {
-	this->cleanInputData();
-	this->setInputData(input);
-
-	unsigned int maxPriority = 3;
-
-	for (unsigned i = maxPriority; i > 0; i--) {
-		this->solveResult(i);
-		/* just for remembering what each part of the code does
-			this->solveResult(3); // factorials
-			this->solveResult(2); // powers and roots
-			this->solveResult(1); // multiplication and division
-			this->solveResult(0); // addition subtraction
-		*/
-	}
-	
-	// expected output after the for cycle should be class variable vector<string> containing only one item = result
-
-}
-
 void Parser::cleanInputData() {
 	while (!this->inputData.empty()) {
 		this->inputData.pop_back();
 	}
 }
 
-void Parser::solveResult(unsigned int priority) {
+void Parser::solveResult(int priority) {
 	if (priority == -1) {
-		std::string operation = this->inputData.at(0);
-		this->inputData.erase(this->inputData.begin());
+		std::string operation = this->getInputData().at(0);
+		this->inputData.erase(this->getInputData().begin());
 		// TODO conversion of inputData vector<string> to vector<double> should happen here
 		std::string str;
 		if (operation == "SUM") {
-			str = std::to_string(this->sum(inputData));
+			str = std::to_string(this->sum(this->getInputData()));
 			this->setInputData({str});
 		} else if (operation == "AVG") {
-			str = std::to_string(this->average(inputData));
+			str = std::to_string(this->average(this->getInputData()));
 			this->setInputData({str});
 		} else if (operation == "DEV") {
-			str = std::to_string(this->deviation(inputData));
+			str = std::to_string(this->deviation(this->getInputData()));
 			this->setInputData({str});
 		}
 	} else {
 		for (unsigned int i = 0; i < this->inputData.size(); i++) {
-			string str;
+			std::string str;
 			if (priority == 3) {
-				if (this->inputData.at(i) == "!") {
-					this->inputData.at(i-1) = std::to_string(this->factorial(stod(this->inputData.at(i-1))));
-					this->inputData.erase(this->inputData.begin()+i);
+				if (this->getInputData().at(i) == "!") {
+					this->getInputData().at(i-1) = std::to_string(this->factorial(stoi(this->getInputData().at(i-1))));
+					this->getInputData().erase(this->getInputData().begin()+i);
 				}
 			} else if (priority == 2) {
 
@@ -69,8 +49,8 @@ void Parser::solveResult(unsigned int priority) {
 	}
 }
 
-string Parser::solve(std::vector<std::string> &input, unsigned int type) {
-		this->cleanInputData();
+std::string Parser::solve(std::vector<std::string> &input, unsigned int type) {
+	this->cleanInputData();
 	this->setInputData(input);
 
 	unsigned int maxPriority = 3;
