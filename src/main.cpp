f62bf9c0ffa508fe20e55ad845256dec381f4f1c
Dominik Skála <DominikSkala@seznam.cz>
2018-03-24 18:13:35 +0100

    renamed Parser class to Calculator

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,15 @@
 
 int main() {
 	
-	Parser Calculator;
+	Calculator Calculator1;
 	
-	std::cout << Calculator.addition(1.0, 2.0) << std::endl;
-	std::cout << Calculator.subtraction(5, 2) << std::endl;
-	std::cout << Calculator.multiplication(5, 2) << std::endl;
-	std::cout << Calculator.division(6, 2) << std::endl;
-	std::cout << Calculator.powerOf(5, 2) << std::endl;
-	std::cout << Calculator.factorial(5) << std::endl;
-	std::cout << Calculator.root(81, 4) << std::endl;
+	std::cout << Calculator1.addition(1.0, 2.0) << std::endl;
+	std::cout << Calculator1.subtraction(5, 2) << std::endl;
+	std::cout << Calculator1.multiplication(5, 2) << std::endl;
+	std::cout << Calculator1.division(6, 2) << std::endl;
+	std::cout << Calculator1.powerOf(5, 2) << std::endl;
+	std::cout << Calculator1.factorial(5) << std::endl;
+	std::cout << Calculator1.root(81, 4) << std::endl;
 	std::vector<std::string> StringVector(6);
 	StringVector.at(0) = "SUM";
 	StringVector.at(1) = "1";
@@ -24,12 +24,12 @@ int main() {
 	DoubleVector.at(2) = 30;
 	DoubleVector.at(3) = 40;
 	DoubleVector.at(4) = 50;
-	std::cout << Calculator.sum(&DoubleVector) << std::endl;
-	std::cout << Calculator.average(&DoubleVector) << std::endl;
-	std::cout << Calculator.deviation(&DoubleVector) << std::endl;
+	std::cout << Calculator1.sum(&DoubleVector) << std::endl;
+	std::cout << Calculator1.average(&DoubleVector) << std::endl;
+	std::cout << Calculator1.deviation(&DoubleVector) << std::endl;
 	std::cout << "Size of StringVector: " << StringVector.size() << std::endl;
-	std::cout << Calculator.solve(StringVector, 1) << std::endl;
+	std::cout << Calculator1.solve(StringVector, 1) << std::endl;
 	
-	std::cout << "Sample calculator source code" << std::endl;
+	std::cout << "Sample Calculator1 source code" << std::endl;
 	return 0;
 }
