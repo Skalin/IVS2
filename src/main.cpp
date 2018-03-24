97f7e9cc510a14f7e2d58881b12be568d8992413
Dominik Skála <DominikSkala@seznam.cz>
2018-03-24 18:10:34 +0100

    added StringVector for testing solve()

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,13 @@ int main() {
 	std::cout << Calculator.powerOf(5, 2) << std::endl;
 	std::cout << Calculator.factorial(5) << std::endl;
 	std::cout << Calculator.root(81, 4) << std::endl;
+	std::vector<std::string> StringVector(6);
+	StringVector.at(0) = "SUM";
+	StringVector.at(1) = "1";
+	StringVector.at(2) = "1";
+	StringVector.at(3) = "1";
+	StringVector.at(4) = "1";
+	StringVector.at(5) = "1";
 	std::vector<double> DoubleVector (5);
 	DoubleVector.at(0) = 10;
 	DoubleVector.at(1) = 20;
@@ -20,6 +27,8 @@ int main() {
 	std::cout << Calculator.sum(&DoubleVector) << std::endl;
 	std::cout << Calculator.average(&DoubleVector) << std::endl;
 	std::cout << Calculator.deviation(&DoubleVector) << std::endl;
+	std::cout << "Size of StringVector: " << StringVector.size() << std::endl;
+	std::cout << Calculator.solve(StringVector, 1) << std::endl;
 	
 	std::cout << "Sample calculator source code" << std::endl;
 	return 0;
