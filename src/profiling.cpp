ed1b6227e25ed10737f9802de91bec9ae13bd131
Johnczek <janhrboticky@gmail.com>
2018-03-24 12:09:46 +0100

    EDIT: Profilingcpp class init

--- src/profiling.cpp
+++ src/profiling.cpp
@@ -1,7 +1,5 @@
 #include "profiling.h"
 
-int main() {
-	std::cout << "Sample profiling source code" << std::endl;
-	return 0;
-}
+double Profiling::deviation(vector<double> &arrayOfDoubles) {
 
+};
