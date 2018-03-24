2c09b5716dfa729a2b98e00bf49b1d2461e2ce08
kisi8 <juraj.kisi.kysel@gmail.com>
2018-03-24 11:10:23 +0100

    Update parser.h

--- src/parser.h
+++ src/parser.h
@@ -14,7 +14,7 @@ protected:
 	std::vector <std::string> getInputData();
 	void setInputData(std::vector<std::string> &inputData);
 private:
-	std::vector <std::double> convertToDouble(std::vector<std::string> &inputData);
+	std::vector <double> convertToDouble(std::vector<std::string> &inputData);
 	void cleanInputData();
 	void solveResult(int priority);
 	
