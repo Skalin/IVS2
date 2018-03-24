b274f65f1ba62da6288c49d745a6f853e64b97ee
Dominik Skála <DominikSkala@seznam.cz>
2018-03-24 12:01:37 +0100

    class Basic initialization

---
+++ src/basic.h
@@ -0,0 +1,18 @@
+//
+// Created by Dominik on 24-Mar-18.
+//
+
+#include <iostream>
+
+#ifndef IVS2_BASIC_H
+#define IVS2_BASIC_H
+
+
+class Basic {
+public:
+	void printError();
+	void printError(std::string error);
+};
+
+
+#endif //IVS2_BASIC_H
