a863c9411b97bf5155afe69ed49304fc4eab9fe8
Johnczek <janhrboticky@gmail.com>
2018-03-24 12:09:17 +0100

    EDIT: Profiling basic data

--- src/profiling.h
+++ src/profiling.h
@@ -1,10 +1,12 @@
-//
-// Created by Dominik on 20-Mar-18.
-//
-
 #ifndef IVS2_PROFILING_H
 #define IVS2_PROFILING_H
 
 #include<iostream>
+#include "calc.h"
+
+class Profiling : public Math {
+public:
+	double deviation(vector <double> &arrayOfDoubles);
+};
 
 #endif //IVS2_PROFILING_H
