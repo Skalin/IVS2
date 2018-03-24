117fbca33452a4d95a110e3a9ca6dc22fd72af13
Dominik Skála <DominikSkala@seznam.cz>
2018-03-24 12:44:53 +0100

    removed whitespace between vector namespace and type of vector

--- src/parser.h
+++ src/parser.h
@@ -11,12 +11,12 @@ class Parser : public Math {
 public:
 	std::string solve(std::vector<std::string> &input, unsigned int type);
 protected:
-	std::vector <std::string> getInputData();
+	std::vector<std::string> getInputData();
 	void setInputData(std::vector<std::string> &inputData);
 private:
-	std::vector <double> convertToDouble(std::vector<std::string> &inputData);
+	std::vector<double> convertToDouble(std::vector<std::string> &inputData);
 	void cleanInputData();
 	void solveResult(int priority);
-	std::vector <std::string> &inputData;
+	std::vector<std::string> &inputData;
 };
 #endif //IVS2_PARSER_H
