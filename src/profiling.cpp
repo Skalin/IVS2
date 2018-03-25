63b1723afa202a4ab7dcee28f47a7732756e00dd
Johnczek <janhrboticky@gmail.com>
2018-03-25 21:23:01 +0200

    EDIT: profiling deviation method implementation

--- src/profiling.cpp
+++ src/profiling.cpp
@@ -1,5 +1,25 @@
 #include "profiling.h"
 
-double Profiling::deviation(vector<double> &arrayOfDoubles) {
+double Profiling::deviation(std::vector<double> &arrayOfDoubles) {
+    double result;
+    double sumPower;
+    double tmp;
+    double div;
 
+    result = average(arrayOfDoubles);
+    result = powerOf(result, 2);
+    result = multiplication(result, arrayOfDoubles.size());
+
+    for (unsigned int i =0; i < arrayOfDoubles.size(); i++) {
+        tmp = arrayOfDoubles.at(i);
+        tmp = powerOf(tmp, 2);
+        sumPower = addition(sumPower, tmp);
+    }
+
+    result = subtraction(sumPower, result);
+    div = subtraction(arrayOfDoubles.size(), 1);
+    div = division(1, div);
+    result = multiplication(div, result);
+    result = root(result, 2);
+    return result;
 };
