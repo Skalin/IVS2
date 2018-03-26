ab558cc34646a0283c5d25b6684de963c0081c8c
kisi8 <juraj.kisi.kysel@gmail.com>
2018-03-26 17:25:35 +0200

    doxygen - class profiling

--- src/profiling.h
+++ src/profiling.h
@@ -3,6 +3,10 @@
 #ifndef IVS2_PROFILING_H
 #define IVS2_PROFILING_H
 
+//! Class for profiling.
+/*!
+	Class for profiling according to sample standard deviation.
+*/
 class Profiling : public Math {
 public:
 	double deviation(std::vector <double> *arrayOfDoubles);
