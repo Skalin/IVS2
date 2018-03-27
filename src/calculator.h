1de966f0a24c1b5035da1c1af82a3609f81bc1c3
Dominik Skála <DominikSkala@seznam.cz>
2018-03-27 15:08:18 +0200

    doxygen review of calculator (removed dots at the end of comments)

--- src/calculator.h
+++ src/calculator.h
@@ -3,25 +3,25 @@
 
 #include "profiling.h"
 
-//! Class representing calculator.
+//! Class representing calculator
 /*!
 	Class inheriting classes Basic, Math and Profiling
 	\sa Basic, Math, Profiling
 */
 class Calculator : public Profiling {
 public:
-	//! Constructor.
+	//! Constructor
 	/*!
-		Function for initializing class variables.
+		Function for initializing class variables
 	*/
 	Calculator();
 	
-	//! Calculation of problem.
+	//! Calculation of problem
 	/*!
-		Function for calculation of given problem.
+		Function for calculation of given problem
 		Manages the overall logic of the solution
-		\param std::vector<std::string> &input string vector representing the data entered to calculator.
-		\param unsigned type determines the type of operation to be performed.
+		\param std::vector<std::string> &input string vector representing the data entered to calculator
+		\param unsigned type determines the type of operation to be performed
 		\return std::string Result of calculation converted to string
 	*/
 	std::string solve(std::vector<std::string> &input, unsigned int type);
@@ -61,35 +61,35 @@ private:
 	*/
 	std::vector<double> &convertToDouble();
 	
-	//! Deleting certain data from string vector.
+	//! Deleting certain data from string vector
 	/*!
-		Function for deleting certain data from string vector inputData.
-		 \param start starting position for deleting.
-		 \param amount number of elements to be deleted.
+		Function for deleting certain data from string vector inputData
+		 \param start starting position for deleting
+		 \param amount number of elements to be deleted
 	*/
 	void deleteItemsFromInputDataVector(unsigned int start, unsigned int amount);
 	
-	//! Deleting all the data from strin vector.
+	//! Deleting all the data from strin vector
 	/*!
-		Function for deleting all the data from string vector inputData.
+		Function for deleting all the data from string vector inputData
 	*/
 	void cleanInputData();
 	
-	//! Deleting all the data from double vector.
+	//! Deleting all the data from double vector
 	/*!
-		Function for deleting all the data from double vector doubleData.
+		Function for deleting all the data from double vector doubleData
 	*/
 	void cleanDoubleData();
 	
-	//! Calculation of mathematic operations based on priority.
+	//! Calculation of mathematic operations based on priority
 	/*!
-		Function for calculating mathematic operations based on operators priority.
-		\param priority priority of operation.
+		Function for calculating mathematic operations based on operators priority
+		\param priority priority of operation
 	*/
 	void solveResult(int priority);
 	
-	std::vector<std::string> inputData; /*!< String vector representing data entered into calculator. */ 
+	std::vector<std::string> inputData; /*!< String vector representing data entered into calculator */
 	
-	std::vector<double> doubleData; /*!< Double vector representing data entered into calculator as double. */
+	std::vector<double> doubleData; /*!< Double vector representing data entered into calculator as double */
 };
 #endif //IVS2_PARSER_H
