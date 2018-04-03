d2503866caa5e1362b5e79cd58ec3d3c83e36000
Skalin <DominikSkala@seznam.cz>
2018-04-03 17:42:21 +0200

    files needed for compilation of Qt

---
+++ src/mainwindow.cpp
@@ -0,0 +1,353 @@
+//! Implementation of functions for user interface logic
+/*! \file mainwindow.cpp
+ *
+ */
+
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    //ui->resultArea->setInputMask("9999999");
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_numberSeven_clicked()
+{
+    ui->resultArea->setText(ui->resultArea->text()+ "7");
+}
+
+
+void MainWindow::on_allClear_clicked()
+{
+    ui->resultArea->setText("");
+}
+
+
+void MainWindow::on_deleteButton_clicked()
+{
+    QString tmp;
+    tmp = ui->resultArea->text();
+    tmp.resize(tmp.size()-1);
+    ui->resultArea->setText(tmp);
+}
+
+void MainWindow::on_numberEight_clicked()
+{
+    ui->resultArea->setText(ui->resultArea->text()+ "8");
+}
+
+void MainWindow::on_numberNine_clicked()
+{
+    ui->resultArea->setText(ui->resultArea->text()+ "9");
+}
+
+void MainWindow::on_numberFour_clicked()
+{
+   ui->resultArea->setText(ui->resultArea->text()+ "4");
+}
+
+void MainWindow::on_numberFive_clicked()
+{
+    ui->resultArea->setText(ui->resultArea->text()+ "5");
+}
+
+void MainWindow::on_numberSix_clicked()
+{
+    ui->resultArea->setText(ui->resultArea->text()+ "6");
+}
+
+void MainWindow::on_numberOne_clicked()
+{
+    ui->resultArea->setText(ui->resultArea->text()+ "1");
+}
+
+void MainWindow::on_numberTwo_clicked()
+{
+    ui->resultArea->setText(ui->resultArea->text()+ "2");
+}
+
+void MainWindow::on_numberThree_clicked()
+{
+    ui->resultArea->setText(ui->resultArea->text()+ "3");
+}
+
+void MainWindow::on_numberZero_clicked()
+{
+     ui->resultArea->setText(ui->resultArea->text()+ "0");
+}
+
+void MainWindow::on_decimalPoint_clicked()
+{
+     ui->resultArea->setText(ui->resultArea->text()+ ".");
+}
+
+void MainWindow::on_sqrtSign_clicked()
+{
+    ui->resultArea->setText(ui->resultArea->text()+ "√");
+}
+
+void MainWindow::on_sqrSign_clicked()
+{
+    ui->resultArea->setText(ui->resultArea->text()+ "^");
+}
+
+void MainWindow::on_factorialSign_clicked()
+{
+    ui->resultArea->setText(ui->resultArea->text()+ "!");
+}
+
+void MainWindow::on_divideSign_clicked()
+{
+     ui->resultArea->setText(ui->resultArea->text()+ "/");
+}
+
+void MainWindow::on_multiplySign_clicked()
+{
+    ui->resultArea->setText(ui->resultArea->text()+ "*");
+}
+
+void MainWindow::on_minusSign_clicked()
+{
+     ui->resultArea->setText(ui->resultArea->text()+ "-");
+}
+
+void MainWindow::on_plusSign_clicked()
+{
+    ui->resultArea->setText(ui->resultArea->text()+ "+");
+}
+
+/*@brief forbids invalid characters in input*/
+void MainWindow::on_resultArea_textChanged(const QString &arg1)
+{
+    QString tmp;
+    tmp = arg1;
+    //input restrictons
+    tmp.remove(QRegExp("[^0123456789./*+!√^-,]"));
+
+    //seg fault
+    if (tmp.size()>1){
+        //+-
+        if (tmp.at(tmp.size()-2) == '+' && tmp.at(tmp.size()-1) == '-'){
+            on_deleteButton_clicked();
+            on_deleteButton_clicked();
+            ui->resultArea->setText(ui->resultArea->text()+ "-");
+        }
+        //-+
+        if (tmp.at(tmp.size()-2) == '-' && tmp.at(tmp.size()-1) == '+'){
+            on_deleteButton_clicked();
+            on_deleteButton_clicked();
+            ui->resultArea->setText(ui->resultArea->text()+ "+");
+        }
+        //--
+        if (tmp.at(tmp.size()-2) == '-' && tmp.at(tmp.size()-1) == '-'){
+            on_deleteButton_clicked();
+        }
+        //++
+        if (tmp.at(tmp.size()-2) == '+' && tmp.at(tmp.size()-1) == '+'){
+            on_deleteButton_clicked();
+        }
+
+    }
+
+}
+std::vector<std::string> MainWindow::splitInputString(std::string input, char splitter, std::string mode){
+    std::vector<std::string> listKeywords = {};
+    if (!mode.empty()){
+        listKeywords.push_back(mode);
+    }
+    std::string tmp = "";
+    for (unsigned int i=0; i<input.length();i++){
+        if (input.at(i) == splitter){
+            tmp.erase(std::remove(tmp.begin(), tmp.end(), ','), tmp.end());
+            if (!tmp.empty()){
+                listKeywords.push_back(tmp);
+                tmp = "";
+            }
+        }else{
+            tmp = tmp + input[i];
+        }
+
+    }
+    //push last member but only if not ','
+    if (!tmp.empty() && tmp.find(',') == std::string::npos){
+        listKeywords.push_back(tmp);
+    }
+    return listKeywords;
+}
+
+bool MainWindow::checkInput(QString input, int mode){
+    std::string tmp = input.toStdString();
+    QMessageBox msgBox;
+    QString last = input.at(input.size()-1);
+
+    //general condition
+    if (last == "+" || last == "-" || last =="/" || last == "*" || last == "√" || last == "^"){
+        msgBox.setText("Špatný vstup!\n"
+                       "vstup nemůže být zakončen operátorem\n"
+                       "př. 5+5");
+        msgBox.exec();
+        return false;
+    }
+    //mode specific condition
+    if (mode == 1){
+        if (tmp.find(',') == std::string::npos){
+            msgBox.setText("Špatný vstup!\n"
+                           "členove se oddělují čárkamy\n"
+                           "př. 5,5,5");
+            msgBox.exec();
+            return false;
+        }
+    }else{
+
+    }
+
+    return true;
+}
+
+
+void MainWindow::on_equalSign_clicked()
+{
+    QString tmp;
+    Calculator Calculator1;
+    tmp = ui->resultArea->text();
+    std::vector<std::string> afterSplitting = {};
+
+    //prepare for spliting
+    tmp.replace("+"," + ");
+    tmp.replace("-"," - ");
+    tmp.replace("*"," * ");
+    tmp.replace("/"," / ");
+    tmp.replace("+"," + ");
+    tmp.replace("√"," √ ");
+    tmp.replace("^"," ^ ");
+    tmp.replace("!"," ! ");
+
+     //debug
+     qDebug() << tmp << endl;
+
+     if (!checkInput(tmp,0)){
+         qDebug() << "test" << endl;
+         return;
+     }
+
+     afterSplitting = splitInputString(tmp.toStdString(),' ',"");
+     //debug
+     for(auto &i: afterSplitting)
+         qDebug() << QString::fromStdString(i) << endl;
+
+    //placeholder
+     qDebug() << "tady" << endl;
+     qDebug() << QString::fromStdString(Calculator1.solve(afterSplitting,0)) << endl;
+     ui->resultArea->setText(QString::fromStdString(Calculator1.solve(afterSplitting,0)));
+}
+
+void MainWindow::on_profiling_clicked()
+{
+    QString tmp;
+    Calculator Calculator1;
+    tmp = ui->resultArea->text();
+    std::vector<std::string> afterSplitting = {};
+
+    //debug
+    qDebug() << tmp << endl;
+
+    if (!checkInput(tmp,1)){
+        qDebug() << "test" << endl;
+        return;
+    }
+
+    afterSplitting = splitInputString(tmp.toStdString(),',',"DEV");
+    //debug
+    for(auto &i: afterSplitting)
+        qDebug() << QString::fromStdString(i) << endl;
+
+    //placeholder
+   ui->resultArea->setText(QString::fromStdString(Calculator1.solve(afterSplitting,1)));
+}
+
+void MainWindow::on_commaButton_clicked()
+{
+    ui->resultArea->setText(ui->resultArea->text()+ ",");
+}
+
+void MainWindow::on_average_clicked()
+{
+    QString tmp;
+    Calculator Calculator1;
+    tmp = ui->resultArea->text();
+    std::vector<std::string> afterSplitting = {};
+
+    //debug
+    qDebug() << tmp << endl;
+
+    if (!checkInput(tmp,1)){
+        qDebug() << "test" << endl;
+        return;
+    }
+
+    afterSplitting = splitInputString(tmp.toStdString(),',',"AVG");
+    //debug
+    for(auto &i: afterSplitting)
+        qDebug() << QString::fromStdString(i) << endl;
+
+    //placeholder
+   ui->resultArea->setText(QString::fromStdString(Calculator1.solve(afterSplitting,1)));
+}
+
+void MainWindow::on_sum_clicked()
+{
+    QString tmp;
+    Calculator Calculator1;
+    tmp = ui->resultArea->text();
+    std::vector<std::string> afterSplitting = {};
+
+    //debug
+    qDebug() << tmp << endl;
+
+    if (!checkInput(tmp,1)){
+        qDebug() << "test" << endl;
+        return;
+    }
+
+    afterSplitting = splitInputString(tmp.toStdString(),',',"SUM");
+    //debug
+    for(auto &i: afterSplitting)
+        qDebug() << QString::fromStdString(i) << endl;
+
+    //placeholder
+   ui->resultArea->setText(QString::fromStdString(Calculator1.solve(afterSplitting,1)));
+}
+
+void MainWindow::on_helpButton_clicked()
+{
+    QMessageBox msgBox;
+    msgBox.setTextFormat(Qt::RichText);
+    msgBox.setText("Vysvětlení jednotlivých tlačítek    ");
+    msgBox.setInformativeText("<b>DEL</b>    smaže poslední znak ze vstupu<br>"
+                              "<b>AC</b>    vymaže celý vstup<br>"
+                              "<br>"
+                              "Pro následující operace se členové odělují čárkou (,)<br>"
+                              "<br>"
+                              "<b>σ</b>     funkce pro profiling<br>"
+                              "<b>μ</b>     funkce pro průměr<br>"
+                              "<b>Σ</b>     funkce pro sumu (součet všech prvků)<br>");
+    msgBox.setDetailedText("Příklady použití jednotlivých funkcí\n"
+                           "μ\n"
+                           "5,5,5   => 5\n"
+                           "1,2,3   => 2\n"
+                           "Σ\n"
+                           "5,5,5   = 15\n"
+                           "5,-5,-2 = -2\n"
+                           "σ\n"
+                           "TODO");
+    msgBox.exec();
+
+}
---
+++ src/mainwindow.h
@@ -0,0 +1,238 @@
+//! Header file containing the declaration of class for interacting with calculator interface
+/*! \file mainwindow.h
+ *
+ */
+
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QApplication>
+#include <QMessageBox>
+#include <QDebug>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+
+#include "basic.h"
+#include "calc.h"
+#include "calculator.h"
+#include "profiling.h"
+
+//! User interface namespace
+/*!
+    Namespace for the class representing calculator.
+*/
+namespace Ui {
+class MainWindow;
+}
+
+//! calculator interface logic class
+/*!
+    Class containing functions for interacting with calculator interface
+*/
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    //! \brief Redefinition of constructor
+    /*!
+        Function for constructing the calculator.
+        \param parent Defining the number of possible windows
+    */
+    explicit MainWindow(QWidget *parent = 0);
+
+    //! \brief Destructor
+    /*!
+        Function for destroying the calculator.
+    */
+    ~MainWindow();
+
+private slots:
+    //! \brief Appends '7' to input
+    /*!
+        Function for appending '7' to input
+    */
+    void on_numberSeven_clicked();
+
+    //! \brief Clears everything from input
+    /*!
+        Function for clearing everything from input
+    */
+    void on_allClear_clicked();
+
+    //! \brief Deletes last character from input
+    /*!
+        Function for deleting last character from input
+    */
+    void on_deleteButton_clicked();
+
+    //! \brief Appends '8' to input
+    /*!
+        Function for appending '8' to input
+    */
+    void on_numberEight_clicked();
+
+    //! \brief Appends '9' to input
+    /*!
+        Function for appending '9' to input
+    */
+    void on_numberNine_clicked();
+
+    //! \brief Appends '4' to input
+    /*!
+        Function for appending '4' to input
+    */
+    void on_numberFour_clicked();
+
+    //! \brief Appends '5' to input
+    /*!
+        Function for appending '5' to input
+    */
+    void on_numberFive_clicked();
+
+    //! \brief Appends '6' to input
+    /*!
+        Function for appending '6' to input
+    */
+    void on_numberSix_clicked();
+
+    //! \brief Appends '1' to input
+    /*!
+        Function for appending '1' to input
+    */
+    void on_numberOne_clicked();
+
+    //! \brief Appends '2' to input
+    /*!
+        Function for appending '2' to input
+    */
+    void on_numberTwo_clicked();
+
+    //! \brief Appends '3' to input
+    /*!
+        Function for appending '3' to input
+    */
+    void on_numberThree_clicked();
+
+    //! \brief Appends '0' to input
+    /*!
+        Function for appending '0' to input
+    */
+    void on_numberZero_clicked();
+
+    //! \brief Appends '.' to input
+    /*!
+        Function for appending '.' to input
+    */
+    void on_decimalPoint_clicked();
+
+    //! \brief Appends '√' to input
+    /*!
+        Function for appending '√' to input
+    */
+    void on_sqrtSign_clicked();
+
+    //! \brief Appends '^' to input
+    /*!
+        Function for appending '^' to input
+    */
+    void on_sqrSign_clicked();
+
+    //! \brief Appends '!' to input
+    /*!
+        Function for appending '!' to input
+    */
+    void on_factorialSign_clicked();
+
+    //! \brief Appends '/' to input
+    /*!
+        Function for appending '/' to input
+    */
+    void on_divideSign_clicked();
+
+    //! \brief Appends '*' to input
+    /*!
+        Function for appending '*' to input
+    */
+    void on_multiplySign_clicked();
+
+    //! \brief Appends 'i' to input
+    /*!
+        Function for appending 'i' to input
+    */
+    void on_minusSign_clicked();
+
+    //! \brief Appends '+' to input
+    /*!
+        Function for appending '+' to input
+    */
+    void on_plusSign_clicked();
+
+    //! \brief Forbids invalid characters in input
+    /*!
+        Function for forbiding invalid characters in input
+        \param arg1 representing input string (sequence)
+    */
+    void on_resultArea_textChanged(const QString &arg1);
+
+
+    //! Input splitter
+    /*! \brief Converts string input into vector
+        \param input input string that needs to be split
+        \param splitter character to split the input with
+        \return vector of strings
+    */
+   std::vector<std::string> splitInputString(std::string input, char splitter, std::string mode);
+
+    //! \brief Prints out result of calculation
+    /*!
+        Function for printing out the result of calculation
+    */
+    void on_equalSign_clicked();
+
+    //! \brief Prints out result of profiling
+    /*!
+        Function for printing out the result of profiling
+    */
+    void on_profiling_clicked();
+
+    //! \brief Appends ',' to input
+    /*!
+        Function for appending ',' to input
+    */
+    void on_commaButton_clicked();
+
+    //! \brief Handles average button
+    /*!
+        Function for calling sum function
+    */
+    void on_average_clicked();
+
+    //! \brief Handles sum button
+    /*!
+        Function for calling average function
+    */
+    void on_sum_clicked();
+
+    //! \brief Shows help
+    /*!
+        Function for showing help
+    */
+    void on_helpButton_clicked();
+
+
+    //! \brief checks input
+    /*! \param input string to check
+        \param mode for wich operation its checking
+        \return true if input is ok
+    */
+    bool checkInput(QString input, int mode);
+
+private:
+    Ui::MainWindow *ui; /*!< Instance variable of class */
+};
+
+#endif // MAINWINDOW_H
