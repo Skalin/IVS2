11403788afece7c9d6f953dd04c9e5fbf24df5b2
Skalin <DominikSkala@seznam.cz>
2018-04-10 19:48:07 +0200

    header file with class declaration

---
+++ src/profilingApplication.h
@@ -0,0 +1,27 @@
+//! Header file containing declaration of profiling functionality for Profiler functionality
+/*! \file profilingApplication.cpp
+ *
+ */
+
+
+#ifndef IVS2_PROFILINGAPPLICATION_H
+#define IVS2_PROFILINGAPPLICATION_H
+
+#include "profiling.h"
+
+class ProfilingApplication : public Profiling {
+public:
+	ProfilingApplication(std::string input);
+	~ProfilingApplication();
+	std::string getInput();
+	std::vector<double>& getDoubleVector();
+	void convertStringToDoubleVector();
+protected:
+private:
+	void setInput(std::string input);
+	std::string input;
+	std::vector<double> vectorOfDoubles;
+	void setDoubleVector(std::vector<double> &);
+};
+
+#endif //IVS2_PROFILINGAPPLICATION_H
