267f0a86f478c925a0297a8b248aed35a1ff6dbf
Jan Válka <xvalka03@stud.fit.vutbr.cz>
2018-04-19 15:04:52 +0200

    changed help

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -408,11 +408,11 @@ void MainWindow::on_helpButton_clicked()
     msgBox.setTextFormat(Qt::RichText);
     msgBox.setText("Vysvětlení jednotlivých tlačítek    ");
     msgBox.setInformativeText("<b>DEL</b>    smaže poslední znak ze vstupu<br>"
-                              "<b>AC</b>    vymaže celý vstup<br>"
+                              "<b>AC</b>    vymaže celý vstup (možné zavolat klávesou Page Down)<br>"
                               "<br>"
                               "Pro následující operace se členové odělují čárkou (,)<br>"
                               "<br>"
-                              "<b>σ</b>     funkce pro profiling<br>"
+                              "<b>σ</b>     funkce pro směrodatnou odchylku<br>"
                               "<b>μ</b>     funkce pro průměr<br>"
                               "<b>Σ</b>     funkce pro sumu (součet všech prvků)<br>");
     msgBox.setDetailedText("Příklady použití jednotlivých funkcí\n"
